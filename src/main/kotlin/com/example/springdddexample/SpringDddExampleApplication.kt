5e59dfac57a9fa6af127e5665ea2470ae8bb18ab
funakihirokazu <hirokazu_funaki@yahoo.co.jp>
2025-10-21 08:40:12 +0900

    Spring Boot を追加

---
+++ src/main/kotlin/com/example/springdddexample/SpringDddExampleApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springdddexample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringDddExampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringDddExampleApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/springdddexample/SpringDddExampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.springdddexample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringDddExampleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
